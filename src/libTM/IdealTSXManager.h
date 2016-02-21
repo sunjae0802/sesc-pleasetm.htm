e0ae968b8eab6cbe7b22bc147cd1cde6fe36e7b1
Sunjae Park <sunjae.park@gatech.edu>
2016-02-20 22:19:02 -0500

    Split various HTM types into separate files

---
+++ src/libTM/IdealTSXManager.h
@@ -0,0 +1,40 @@
+#ifndef IDEAL_TSX_MANAGER
+#define IDEAL_TSX_MANAGER
+
+#include <set>
+#include <vector>
+#include "TMCoherence.h"
+#include "PrivateCache.h"
+
+class IdealTSXManager: public HTMManager {
+public:
+    IdealTSXManager(const char tmStyle[], int32_t nCores, int32_t line);
+    virtual ~IdealTSXManager();
+
+    typedef CacheAssocTM    Cache;
+    typedef TMLine          Line;
+protected:
+    virtual TMRWStatus TMRead(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
+    virtual TMRWStatus TMWrite(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
+    virtual void       nonTMRead(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
+    virtual void       nonTMWrite(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
+    virtual TMBCStatus myAbort(InstDesc* inst, const ThreadContext* context, InstContext* p_opStatus);
+    virtual TMBCStatus myCommit(InstDesc* inst, const ThreadContext* context, InstContext* p_opStatus);
+
+    // Helper functions
+    Cache* getCache(Pid_t pid) { return caches.at(pid/nSMTWays); }
+    Line* replaceLine(Pid_t pid, VAddr raddr);
+    void cleanDirtyLines(Pid_t pid, VAddr caddr, std::set<Cache*>& except);
+    void invalidateLines(Pid_t pid, VAddr caddr, std::set<Cache*>& except);
+    void abortTMWriters(Pid_t pid, VAddr caddr, bool isTM, std::set<Cache*>& except);
+    void abortTMSharers(Pid_t pid, VAddr caddr, bool isTM, std::set<Cache*>& except);
+
+    // Configurable member variables
+    int             totalSize;
+    int             assoc;
+
+    // State member variables
+    std::vector<Cache*>         caches;
+};
+
+#endif
--- src/libTM/TMCoherence.h
+++ src/libTM/TMCoherence.h
@@ -10,8 +10,6 @@
 #include "GStats.h"
 #include "libemul/InstDesc.h"
 #include "TMState.h"
-#include "PrivateCache.h"
-#include "HWGate.h"
 
 #define MAX_CPU_COUNT 512
 
@@ -129,72 +127,6 @@ protected:
     std::map<Pid_t, std::set<VAddr> >   linesWritten;
     std::map<VAddr, std::set<Pid_t> >   writers;
     std::map<VAddr, std::set<Pid_t> >   readers;
-};
-
-class IdealTSXManager: public HTMManager {
-public:
-    IdealTSXManager(const char tmStyle[], int32_t nCores, int32_t line);
-    virtual ~IdealTSXManager();
-
-    typedef CacheAssocTM    Cache;
-    typedef TMLine          Line;
-protected:
-    virtual TMRWStatus TMRead(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual TMRWStatus TMWrite(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual void       nonTMRead(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual void       nonTMWrite(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual TMBCStatus myAbort(InstDesc* inst, const ThreadContext* context, InstContext* p_opStatus);
-    virtual TMBCStatus myCommit(InstDesc* inst, const ThreadContext* context, InstContext* p_opStatus);
-
-    // Helper functions
-    Cache* getCache(Pid_t pid) { return caches.at(pid/nSMTWays); }
-    Line* replaceLine(Pid_t pid, VAddr raddr);
-    void cleanDirtyLines(Pid_t pid, VAddr caddr, std::set<Cache*>& except);
-    void invalidateLines(Pid_t pid, VAddr caddr, std::set<Cache*>& except);
-    void abortTMWriters(Pid_t pid, VAddr caddr, bool isTM, std::set<Cache*>& except);
-    void abortTMSharers(Pid_t pid, VAddr caddr, bool isTM, std::set<Cache*>& except);
-
-    // Configurable member variables
-    int             totalSize;
-    int             assoc;
-
-    // State member variables
-    std::vector<Cache*>         caches;
-};
-
-class TSXManager: public HTMManager {
-public:
-    TSXManager(const char tmStyle[], int32_t nCores, int32_t line);
-    virtual ~TSXManager();
-
-    typedef CacheAssocTM    Cache;
-    typedef TMLine          Line;
-protected:
-    virtual TMRWStatus TMRead(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual TMRWStatus TMWrite(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual void       nonTMRead(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual void       nonTMWrite(InstDesc* inst, const ThreadContext* context, VAddr raddr, InstContext* p_opStatus);
-    virtual TMBCStatus myAbort(InstDesc* inst, const ThreadContext* context, InstContext* p_opStatus);
-    virtual TMBCStatus myCommit(InstDesc* inst, const ThreadContext* context, InstContext* p_opStatus);
-
-    // Helper functions
-    Cache* getCache(Pid_t pid) { return caches.at(pid/nSMTWays); }
-    void getPeers(Pid_t pid, std::set<Pid_t>& peers);
-    void updateOverflow(Pid_t pid, VAddr newCaddr);
-    Line* replaceLine(Pid_t pid, VAddr raddr);
-    void cleanDirtyLines(Pid_t pid, VAddr caddr, std::set<Cache*>& except);
-    void invalidateLines(Pid_t pid, VAddr caddr, std::set<Cache*>& except);
-    void abortTMWriters(Pid_t pid, VAddr caddr, bool isTM, std::set<Cache*>& except);
-    void abortTMSharers(Pid_t pid, VAddr caddr, bool isTM, std::set<Cache*>& except);
-
-    // Configurable member variables
-    int             totalSize;
-    int             assoc;
-    size_t          maxOverflowSize;
-
-    // State member variables
-    std::vector<Cache*>         caches;
-    std::map<Pid_t, std::set<VAddr> >   overflow;
 };
 
 extern HTMManager *htmManager;
