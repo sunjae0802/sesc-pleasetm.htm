88c7c08f99d826d3af22d72cbcf1700f48a6e22e
Sunjae Park <sunjae.park@gatech.edu>
2016-04-14 11:01:42 -0400

    Cleanup TMStorage

--- src/libTM/TMContext.h
+++ src/libTM/TMContext.h
@@ -37,48 +37,20 @@ private:
     Pid_t       pid;          // Copy of PID of owner thread
     uint64_t    utid;         // Unique transaction identifier
     RegVal      regs[NumOfRegs];      // Int Register Backup
-    TMStorage   cache;        // The Memory Cache
     TMStorage2  cache2;        // The Memory Cache
     TMContext  *parent;      // Parent Transaction
 };
 
 template<class T>
 void TMContext::cacheAccess(VAddr addr, T oval, T* p_val) {
-#if 0
-    T val = oval;
-    bool found = false;
-    cache.loadLine(context, addr);
-
-    if(sizeof(T) == 1) {
-        val = (T)cache.load8(addr, &found);
-    } else if(sizeof(T) == 2) {
-        val = (T)cache.load16(addr, &found);
-    } else if(sizeof(T) == 4) {
-        val = (T)cache.load32(addr, &found);
-    } else if(sizeof(T) == 8) {
-        val = (T)cache.load64(addr, &found);
-    } else {
-        assert(0);
-    }
-#endif
     cache2.loadLine(context, addr);
     T newval = cache2.load<T>(addr);
 
-#if 0
-    if (!found) {
-        if(oval != newval) { fail("0x%lx != 0x%lx\n", oval, newval); }
-    } else {
-        if(val != newval) { fail("0x%lx != 0x%lx\n", val, newval); }
-    }
-#endif
     *p_val = newval;
 }
 
 template<class T>
 void TMContext::cacheWrite(VAddr addr, T val) {
-    //cache.loadLine(context, addr);
-    //cache.store(context, addr, val);
-
     cache2.loadLine(context, addr);
     cache2.store<T>(context, addr, val);
 }
--- src/libTM/TMStorage.h
+++ src/libTM/TMStorage.h
@@ -2,83 +2,9 @@
 #define TM_CACHE
 
 #include <map>
-#include "SescConf.h"
 #include "libemul/Addressing.h"
-//#include "mendian.h"
-#include "libemul/Regs.h"
 class ThreadContext;
 
-/*
- * #define SWAP_WORD(X) (((((unsigned int)(X)) >> 24) & 0x000000ff) | \
-                ((((unsigned int)(X)) >>  8) & 0x0000ff00) | \
-                             ((((unsigned int)(X)) <<  8) & 0x00ff0000) | \
-                             ((((unsigned int)(X)) << 24) & 0xff000000))
-
-
-#define SWAP_SHORT(X) ( ((((unsigned short)X)& 0xff00) >> 8) | ((((unsigned short)X)& 0x00ff) << 8) )
-*/
-
-//typedef int32_t RegVal;
-
-
-typedef uint32_t MEMUNIT;
-
-class TMStorage {
-  const static size_t CACHE_SIZE = 64;
-  struct CacheLine {
-    uint8_t data[CACHE_SIZE];
-  };
-  VAddr computeCAddr(VAddr addr) {
-      return ((addr / CACHE_SIZE) * CACHE_SIZE);
-  }
-  size_t computeCOffset(VAddr addr) {
-    return addr % CACHE_SIZE;
-  }
-  VAddr recompAddr(VAddr addr) {
-      return computeCAddr(addr) + computeCOffset(addr);
-  }
-
-  public:
-    /* Contructor */
-    TMStorage();
-
-    bool inTnxStorage(VAddr addr) {
-        VAddr cAddr = computeCAddr(addr);
-        TnxStorage::iterator iStorage = tnxStorage.find(cAddr);
-        return iStorage != tnxStorage.end();
-    }
-	uint8_t  load8(VAddr addr, bool *found);
-	uint16_t load16(VAddr addr, bool *found);
-	uint32_t load32(VAddr addr, bool *found);
-	uint64_t load64(VAddr addr, bool *found);
-
-	void store(ThreadContext *context, VAddr addr, int8_t val);
-	void store(ThreadContext *context, VAddr addr, uint8_t val);
-	void store(ThreadContext *context, VAddr addr, int16_t val);
-	void store(ThreadContext *context, VAddr addr, uint16_t val);
-	void store(ThreadContext *context, VAddr addr, int32_t val);
-	void store(ThreadContext *context, VAddr addr, uint32_t val);
-	void store(ThreadContext *context, VAddr addr, int64_t val);
-	void store(ThreadContext *context, VAddr addr, uint64_t val);
-    
-    void loadLine(ThreadContext* context, VAddr addr);
-	void flush(ThreadContext* context);
-
-    /* Deconstructor */
-    ~TMStorage();
-
-  private:
-    uint32_t lower32(uint64_t val) {
-        return  (val & 0x00000000FFFFFFFFLL);
-    }
-    uint32_t upper32(uint64_t val) {
-        return ((val & 0xFFFFFFFF00000000LL) >> 32);
-    }
-     std::map<VAddr, MEMUNIT>     memMap; //!< The Memory Map
-
-     typedef std::map<VAddr, CacheLine> TnxStorage;
-     TnxStorage tnxStorage; //!< Speculative storage
-};
 class TMStorage2 {
   const static size_t CACHE_SIZE = 64;
   struct CacheLine {
