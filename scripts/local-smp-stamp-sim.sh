ce6430fa928daec13a6e32f2eb6435d6c6f4dc5d
Sunjae Park <sunjae.park@gatech.edu>
2016-05-04 11:02:48 -0400

    Adding TMBUILD configurable run scripts

---
+++ scripts/local-smp-stamp-sim.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+ARRAYID=$1
+
+. ${SESCDIR}/scripts/setup_stamp.sh
+. ${SESCDIR}/scripts/setup_sesc.sh
+
+# Arguments (CACHE_ID BENCH_ID TID)
+SEED_BASE=0
+CACHE_ID=$((ARRAYID / 100))
+ARRAYID=$((ARRAYID % 100))
+BENCH_ID=$((ARRAYID / 10))
+benchname=`get_benchname $BENCH_ID`
+T_ID=$((ARRAYID % 10))
+THREADS=( 1 2 4 8 16 )
+nthread=${THREADS[$T_ID]}
+
+NCORES="16"
+l1conf=`get_l1conf $CACHE_ID`
+CONF="smp${NCORES}-${l1conf}.conf"
+
+BENCHCONFIG="$TMBUILD"
+INPUTSIZE="sim"
+
+tmp_name=`mktemp -d`
+pushd ${tmp_name}
+
+cp ${SESCDIR}/confs/${CONF} .
+cp ${SESCDIR}/confs/tsx-trans.conf trans.conf
+
+setup_stamp "$BENCHDIR" "$benchname" "$BENCHCONFIG" "$nthread" "$INPUTSIZE" "$SEED_BASE"
+run_sesc "${SESCDIR}/build.smp/sesc" "$CONF" "${PROG}" "${PROGARGS}"
+
+popd
+# Reinit benchname
+benchname=`get_benchname $BENCH_ID`
+benchrundir_name=`get_benchrundir_name "${benchname}-${nthread}-${l1conf}"`
+mv -n ${tmp_name} ${benchrundir_name}
+echo "Copied to ${benchrundir_name}"
